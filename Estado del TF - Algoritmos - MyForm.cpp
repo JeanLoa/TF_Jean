6a53279088eb66c6f6ba5b0f09393c79fb9b35d2
JeanLoa <165863050+JeanLoa@users.noreply.github.com>
2024-11-20 17:19:18 -0500

    Update Estado del TF - Algoritmos - MyForm.cpp

--- Estado del TF - Algoritmos - MyForm.cpp
+++ Estado del TF - Algoritmos - MyForm.cpp
@@ -1,7 +1,9 @@
-#include "MyForm.h"
+#include "FormMenu.h"
+#include <iostream>
 
 using namespace System::Windows::Forms;
-
 void main() {
-	Application::Run(gcnew TF::MyForm());
+	srand(time(0));
+	Application::EnableVisualStyles();
+	Application::Run(gcnew TF::FormMenu());
 }
